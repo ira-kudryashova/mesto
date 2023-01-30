88c525e3ee111ab2cd0922ae215ab5336694da54
Ira Kudryashova <i.trofimova@icloud.com>
2023-01-31 01:15:41 +0300

    cards.css line11 typo

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 let buttonEdit = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let buttonClose = document.querySelector('.form__close-button');
-let formElement = document.querySelector('.form'); // Воспользуйтесь методом querySelector()
-let nameInput = document.querySelector('.form__item_user_name'); // Воспользуйтесь инструментом .querySelector()
-let jobInput = document.querySelector('.form__item_user_job'); // Воспользуйтесь инструментом .querySelector()
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('.form__item_user_name');
+let jobInput = document.querySelector('.form__item_user_job');
 let nameTitle = document.querySelector('.profile__name');
 let jobTitle = document.querySelector('.profile__job');
 
@@ -11,12 +11,9 @@ function closePopup() {
     popup.classList.remove('popup_opened');
 }
 function handleFormSubmit (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
-     // Получите значение полей jobInput и nameInput из свойства value
+    evt.preventDefault();
 
-    nameTitle.textContent = nameInput.value; // Вставьте новые значения с помощью textContent
+    nameTitle.textContent = nameInput.value;
     jobTitle.textContent = jobInput.value;
 
     closePopup();
