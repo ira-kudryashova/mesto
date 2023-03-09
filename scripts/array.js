e065c8c9ed5e9174bce270a7d16969626bd5fe3b
Ira Kudryashova <i.trofimova@icloud.com>
2023-03-09 14:28:50 +0300

    -

--- scripts/array.js
+++ scripts/array.js
@@ -1,27 +1,27 @@
 // массив карточек
 const initialCards = [
   {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+    name: "Сингапур",
+    link: "https://images.unsplash.com/photo-1600468636011-c75ae69b7fcb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHNpbmdhcG9yZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
   },
   {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+    name: "Сеул",
+    link: "https://images.unsplash.com/photo-1602479185195-32f5cd203559?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDl8fHNldWx8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
   },
   {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+    name: "Флоренция",
+    link: "https://images.unsplash.com/photo-1592089206718-1124ee6d6378?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEzfHxmaXJlbnplfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
   },
   {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+    name: "Копенгаген",
+    link: "https://images.unsplash.com/photo-1583674071999-7ec33b8751d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTJ8fGNvcGVuaGFnZW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
   },
   {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+    name: "Амстердам",
+    link: "https://images.unsplash.com/photo-1622015524070-5ea7caac2643?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGFtc3RlcmRhbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
   },
   {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+    name: "Банкок",
+    link: "https://images.unsplash.com/photo-1583491470869-ca0b9fa90216?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGJhbmdrb2t8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
   },
 ];
